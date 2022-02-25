06213bf3611152b51cd613649effa727a794c268
pbernard006 <pbernard006@iutbayonne.univ-pau.fr>
2022-02-25 16:24:09 +0100

    Création des tables utilisateur et plat

---
+++ model/Plat.js
@@ -0,0 +1,48 @@
+import mongoose from 'mongoose' ;
+
+const PlatSchema = new mongoose.Schema({
+    "auteur": {
+        type: String,
+        required: true
+    },
+    "ingredients": {
+        type: [String],
+        required: true
+    },
+    "type": {
+        type: [String],
+        required: true
+    },
+    "saison":{
+        type: String,
+        required: true ;
+    },
+    "etapes":{
+        type: [String],
+        required: true
+    },
+    "personnes": {
+        type: Number,
+        required: true
+    },
+    "difficulte": {
+        type: Number,
+        required: true
+    },
+    "tempsPreparation": {
+        type: Number,
+        required: true
+    },
+    "tempsCuisson": {
+        type: Number,
+        required: true
+    },
+    "tempsTotal": {
+        type: Number,
+        required: true
+    }
+});
+
+const Plat = mongoose.model("plats", PlatSchema);
+
+export default Plat ;
---
+++ model/Utilisateur.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const UtilisateurSchema = new mongoose.Schema({
+    "pseudo": {
+        type: String,
+        required: true
+    },
+    "nom": {
+        type: String,
+        required: true
+    },
+    "prenom": {
+        type: String,
+        required: true
+    },
+    "mail": {
+        type: String,
+        required: true
+    },
+    "mdp": {
+        type: String,
+        required: true
+    }
+})
