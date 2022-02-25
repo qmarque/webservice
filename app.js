4616eb8f2f2f4b749cfa3c2ad08c288c2a5c4c5b
pbernard006 <pbernard006@iutbayonne.univ-pau.fr>
2022-02-25 15:58:35 +0100

    Connexion à Mongoose

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
+const mongoose = require('mongoose');
+
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 8080
+
+// MongoDB
+mongoose.connect('mongodb+srv://hugobarsacq:hugobarsacq@cluster0.rhjep.mongodb.net/biblicette?retryWrites=true&w=majority',{
+  useNewUrlParser : true,
+  useUnifiedTopology: true
+})
+const db = mongoose.connection;
+db.on("error",console.error.bind(console, "Erreur de connexion à Mongo : "));
+db.once("open", function () {
+  console.log("Connexion à Mongo OK");
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
