bfbd74a99607d2ab2ea64170bce7bee4165f2d00
Quentin Marque <quentinm2000@hotmail.fr>
2022-03-03 17:36:32 +0100

    Ajout de la modification et de la suppression d'un plat

--- routes/platsControleur.js
+++ routes/platsControleur.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-// const ObjectID = require('mongoose').Types.ObjectId;
+const ObjectID = require('mongoose').Types.ObjectId;
 
 const { Plat } = require('../modeles/platsModele');
 
@@ -27,9 +27,49 @@ router.post('/', (req, res) => {
     });
 
     nouveauPlat.save((err, docs) => {
-      if (!err) res.send(docs);
-      else console.log('Error creating new data : ' + err);
+        if (!err) res.send(docs);
+        else console.log('Error creating new data : ' + err);
     })
-  });
+});
+
+  router.put("/:id", (req, res) => {
+    if (!ObjectID.isValid(req.params.id))
+      return res.status(400).send("ID unknow : " + req.params.id)
+    
+    const modifierPlat = {
+        "auteur": req.body.auteur,
+        "ingredients": req.body.ingredients,
+        "type": req.body.type,
+        "saison": req.body.saison,
+        "etapes": req.body.etapes,
+        "nbPersonnes": req.body.nbPersonnes,
+        "difficulte": req.body.difficulte,
+        "tempsPreparation": req.body.tempsPreparation,
+        "tempsCuisson": req.body.tempsCuisson,
+        "tempsTotal": req.body.tempsTotal
+    };
+  
+    Plat.findByIdAndUpdate(
+      req.params.id,
+      { $set: modifierPlat},
+      { new: true },
+      (err, docs) => {
+        if (!err) res.send(docs);
+        else console.log("Update error : " + err);
+      }
+    )
+});
+  
+  router.delete("/:id", (req, res) => {
+    if (!ObjectID.isValid(req.params.id))
+      return res.status(400).send("ID unknow : " + req.params.id)
+    
+      Plat.findByIdAndRemove(
+      req.params.id,
+      (err, docs) => {
+        if (!err) res.send(docs);
+        else console.log("Delete error : " + err);
+      })
+});
 
 module.exports = router;
