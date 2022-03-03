a3c29f3a309604648337241a2d442b4f8b7a2a36
Quentin Marque <quentinm2000@hotmail.fr>
2022-03-03 17:31:48 +0100

    Ajout de la modification et de la suppression d'un utilisateur

--- routes/utilisateursControleur.js
+++ routes/utilisateursControleur.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-// const ObjectID = require('mongoose').Types.ObjectId;
+const ObjectID = require('mongoose').Types.ObjectId;
 
 const { Utilisateur } = require('../modeles/utilisateursModele');
 
@@ -11,7 +11,6 @@ router.get('/', (req, res) => {
       else console.log("Error to get data : " + err);
     })
   });
-
 
 router.post('/', (req, res) => {
   const nouvelUtilisateur = new Utilisateur({
@@ -26,6 +25,41 @@ router.post('/', (req, res) => {
     if (!err) res.send(docs);
     else console.log('Error creating new data : ' + err);
   })
+});
+
+router.put("/:id", (req, res) => {
+  if (!ObjectID.isValid(req.params.id))
+    return res.status(400).send("ID unknow : " + req.params.id)
+  
+  const modifierUtilisateur = {
+    "pseudo": req.body.pseudo,
+    "nom": req.body.nom,
+    "prenom": req.body.prenom,
+    "mail": req.body.mail,
+    "mdp": req.body.mdp
+  };
+
+  Utilisateur.findByIdAndUpdate(
+    req.params.id,
+    { $set: modifierUtilisateur},
+    { new: true },
+    (err, docs) => {
+      if (!err) res.send(docs);
+      else console.log("Update error : " + err);
+    }
+  )
+});
+
+router.delete("/:id", (req, res) => {
+  if (!ObjectID.isValid(req.params.id))
+    return res.status(400).send("ID unknow : " + req.params.id)
+  
+    Utilisateur.findByIdAndRemove(
+    req.params.id,
+    (err, docs) => {
+      if (!err) res.send(docs);
+      else console.log("Delete error : " + err);
+    })
 });
 
 module.exports = router;
