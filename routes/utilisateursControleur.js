2281479fafe22bba434f0359ebfd74d3f8b6b13a
Quentin Marque <quentinm2000@hotmail.fr>
2022-03-04 12:07:20 +0100

    Récupération des données d'un utilisateur par identifiant et ajout des codes HTTP

--- routes/utilisateursControleur.js
+++ routes/utilisateursControleur.js
@@ -7,10 +7,20 @@ const { Utilisateur } = require('../modeles/utilisateursModele');
 
 router.get('/', (req, res) => {
   Utilisateur.find((err, docs) => {
-      if (!err) res.send(docs);
-      else console.log("Error to get data : " + err);
-    })
-  });
+    if (!err) res.send(docs);
+    else console.log("Error to get data : " + err);
+  })
+});
+
+router.get("/:id", (req, res) => {
+  if (!ObjectID.isValid(req.params.id))
+  return res.status(404).send("ID unknow : " + req.params.id)
+  
+  Utilisateur.findById(req.params.id, function(err, docs) {
+    if (!err) res.send(docs);
+    else console.log("Error to get data : " + err);
+  })
+});
 
 router.post('/', (req, res) => {
   const nouvelUtilisateur = new Utilisateur({
@@ -22,14 +32,14 @@ router.post('/', (req, res) => {
   });
 
   nouvelUtilisateur.save((err, docs) => {
-    if (!err) res.send(docs);
+    if (!err) res.status(201).send(docs);
     else console.log('Error creating new data : ' + err);
   })
 });
 
 router.put("/:id", (req, res) => {
   if (!ObjectID.isValid(req.params.id))
-    return res.status(400).send("ID unknow : " + req.params.id)
+    return res.status(404).send("ID unknow : " + req.params.id)
   
   const modifierUtilisateur = {
     "pseudo": req.body.pseudo,
@@ -44,7 +54,7 @@ router.put("/:id", (req, res) => {
     { $set: modifierUtilisateur},
     { new: true },
     (err, docs) => {
-      if (!err) res.send(docs);
+      if (!err) res.status(200).send(docs);
       else console.log("Update error : " + err);
     }
   )
@@ -52,12 +62,12 @@ router.put("/:id", (req, res) => {
 
 router.delete("/:id", (req, res) => {
   if (!ObjectID.isValid(req.params.id))
-    return res.status(400).send("ID unknow : " + req.params.id)
+    return res.status(404).send("ID unknow : " + req.params.id)
   
     Utilisateur.findByIdAndRemove(
     req.params.id,
     (err, docs) => {
-      if (!err) res.send(docs);
+      if (!err) res.status(200).send(docs);
       else console.log("Delete error : " + err);
     })
 });
