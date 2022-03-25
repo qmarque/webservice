8686ce41e4174957206425cdf5fa2b142dc4115c
pbernard006 <pbernard006@iutbayonne.univ-pau.fr>
2022-03-25 11:27:13 +0100

    Utilisation d'une autre API pour récupérer la température actuelle

--- src/modeles/platsModele.js
+++ src/modeles/platsModele.js
@@ -1,54 +1,60 @@
-const mongoose = require('mongoose') ;
+const mongoose = require("mongoose");
 
-const PlatSchema = new mongoose.Schema(
-    {
-        "auteur": {
-            type: String,
-            required: true
-        },
-        "nom": {
-            type: String,
-            required: true
-        },
-        "ingredients": {
-            type: [],
-            required: true
-        },
-        "type": {
-            type: [],
-            required: true
-        },
-        "saison":{
-            type: String,
-            required: true
-        },
-        "etapes":{
-            type: [],
-            required: true
-        },
-        "nbPersonnes": {
-            type: Number,
-            required: true
-        },
-        "difficulte": {
-            type: Number,
-            required: true
-        },
-        "tempsPreparation": {
-            type: Number,
-            required: true
-        },
-        "tempsCuisson": {
-            type: Number,
-            required: true
-        },
-        "tempsTotal": {
-            type: Number,
-            required: true
-        }
-    },
-);
+const PlatSchema = new mongoose.Schema({
+  auteur: {
+    type: String,
+    required: true,
+  },
+  nom: {
+    type: String,
+    required: true,
+  },
+  ingredients: {
+    type: [],
+    required: true,
+  },
+  temperatureMinExt: {
+    type: Number,
+    required: true,
+  },
+  temperatureMaxExt: {
+    type: Number,
+    required: true,
+  },
+  type: {
+    type: [],
+    required: true,
+  },
+  saison: {
+    type: String,
+    required: true,
+  },
+  etapes: {
+    type: [],
+    required: true,
+  },
+  nbPersonnes: {
+    type: Number,
+    required: true,
+  },
+  difficulte: {
+    type: Number,
+    required: true,
+  },
+  tempsPreparation: {
+    type: Number,
+    required: true,
+  },
+  tempsCuisson: {
+    type: Number,
+    required: true,
+  },
+  tempsTotal: {
+    type: Number,
+    required: true,
+  },
+});
 
 const Plat = mongoose.model("plats", PlatSchema);
 
-module.exports = { Plat } ;
+module.exports = { Plat };
--- src/services/plats.service.js
+++ src/services/plats.service.js
@@ -1,8 +1,38 @@
+const fetch = require("node-fetch");
 const { Plat } = require("../modeles/platsModele");
 
 async function recupererLesPlats(req) {
   if (req.query != null) {
-    return Plat.find(req.query);
+    if (req.query.lat && req.query.lon) {
+      //Appel API
+      const reponse = await fetch(
+        "https://api.openweathermap.org/data/2.5/weather?lat=" +
+          req.query.lat +
+          "&lon=" +
+          req.query.lon +
+          "&appid=9543685ef70ae794d21e49ef00692c0b"
+      );
+      const data = await reponse.json();
+
+      const temp = parseFloat(data.main.temp) - 273.15;
+
+      //Requête
+      var parametre = {
+        temperatureMinExt: { $lt: temp },
+        temperatureMaxExt: { $gt: temp },
+      };
+
+      for (p in req.query) {
+        if (p.localeCompare("lat") != 0 && p.localeCompare("lon") != 0) {
+          console.log(p);
+          parametre[p] = req.query[p];
+        }
+      }
+
+      return Plat.find(parametre);
+    } else {
+      return Plat.find(req.query);
+    }
   } else {
     return Plat.find();
   }
@@ -13,10 +43,13 @@ async function recupererUnPlat(id) {
 }
 
 async function creerUnPlat(plat) {
+  console.log(plat);
   const nouveauPlat = new Plat({
     auteur: plat.auteur,
     nom: plat.nom,
     ingredients: plat.ingredients,
+    temperatureMinExt: plat.temperatureMinExt,
+    temperatureMaxExt: plat.temperatureMaxExt,
     type: plat.type,
     saison: plat.saison,
     etapes: plat.etapes,
@@ -26,6 +59,8 @@ async function creerUnPlat(plat) {
     tempsCuisson: plat.tempsCuisson,
     tempsTotal: plat.tempsTotal,
   });
+
+  console.log(nouveauPlat);
 
   return nouveauPlat.save();
 }
