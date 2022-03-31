46063b656b377cf5e45c7e9e6b0b432d28887abd
Quentin Marque <quentinm2000@hotmail.fr>
2022-03-31 14:54:08 +0200

    Mise en places de la documentation avec Swagger pour les routes utilisateurs

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 const app = express();
 require("./src/modeles/dbConfig");
 const jwt = require("jsonwebtoken");
@@ -7,6 +9,31 @@ const utilisateursRouteur = require("./src/routes/utilisateurs.route");
 const platsRouteur = require("./src/routes/plats.route");
 const connexionRouteur = require("./src/routes/app.route");
 const bodyParser = require("body-parser");
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Biblicette",
+      version: "0.1.0",
+      description:
+        "La librairie API du projet biblicette développé avec Express et documenté avec Swagger",
+    },
+    servers: [
+      {
+        url: "http://localhost:8080",
+      },
+    ],
+  },
+  apis: ["./src/routes/*.js "],
+};
+
+const specs = swaggerJsdoc(options);
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(specs)
+);
 
 app.use(bodyParser.json());
 app.use("/utilisateurs", authentifierToken, utilisateursRouteur);
--- src/routes/utilisateurs.route.js
+++ src/routes/utilisateurs.route.js
@@ -2,12 +2,145 @@ const express = require("express");
 const { route } = require("express/lib/application");
 const router = express.Router();
 const utilisateursControleur = require("../controleurs/utilisateurs.controleur");
+const { Utilisateur } = require("../modeles/utilisateursModele")
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *       Utilisateur:
+ *          type: object
+ *          required:
+ *           - pseudo
+ *           - nom
+ *           - prenom
+ *           - mail
+ *           - mdp
+ *          properties:
+*           id:
+*              type: string
+*              description: L'identifiant de l'utilisateur est généré automatiquement.
+*           pseudo:
+*              type: string
+*              description: Le pseudo de l'utilisateur.
+*           nom:
+*              type: string
+*              description: Le nom de l'utilisateur.
+*           prenom:
+*              type: string
+*              description: Le prenom de l'utilisateur.
+*           mail:
+*              type: string
+*              description: Le mail de l'utilisateur.
+*           mdp:
+*              type: string
+*              description: Le mot de passe de l'utilisateur.
+*/
+
+/**
+ * @swagger
+ * tags:
+ *  name: Utilisateurs
+  */ 
+
+/**
+ * @swagger
+ * /utilisateurs:
+ *   get:
+ *       summary: Retourne la liste de tous les utilisateurs
+ *       tags: [Utilisateurs]
+ *       responses:
+ *          200:
+ *            description: La liste des utilisateurs
+ *            content:
+ *              application/json:
+ *                  schema:
+ *                      type: array
+ *                      items:
+ *                           $ref: '#/components/schemas/Utilisateur'
+ */
 
 router.get("/", utilisateursControleur.recupererLesUtilisateurs);
 
+/**
+ * @swagger
+ * /utilisateurs/{id}:
+ *   get:
+ *       summary: Retourne un utilisateur par id
+ *       tags: [Utilisateurs]
+ *       parameters:
+ *          - in: path
+ *            name: id
+ *            schema:
+ *              type: string
+ *            required: true
+ *            description: L'utilisateur id
+ *       responses:
+ *          200:
+ *            description: L'utilisateur par id
+ *            content:
+ *              application/json:
+ *                  schema:
+ *                      $ref: '#/components/schemas/Utilisateur'
+ *          400:
+ *            description: L'utilisateur n'a pas été trouvé
+ */
+
 router.get("/:id", utilisateursControleur.recupererUnUtilisateur);
 
-router.put("/:id", utilisateursControleur.modifierUnUtilisateur);
+/**
+ * @swagger
+ * /utilisateurs/{id}:
+ *   put:
+ *       summary: Modifier un utilisateur par id
+ *       tags: [Utilisateurs]
+ *       parameters:
+ *          - in: path
+ *            name: id
+ *            schema:
+ *              type: string
+ *            required: true
+ *            description: L'utilisateur id
+ *       requestBody:
+ *          required: true
+ *          content:
+ *            application/json:
+ *                schema:
+ *                    $ref: '#/components/schemas/Utilisateur'
+ *          responses:
+ *            200:
+ *              description: L'utilisateur a été modifié
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/Utilisateur'
+ *            404:
+ *              description: L'utilisateur n'a pas été trouvé
+ *            500:
+ *              description: Des erreurs sont survenues
+ */
+
+router.put("/:id", utilisateursControleur.modifierUnUtilisateur);  
+
+/**
+ * @swagger
+ * /utilisateurs/{id}:
+ *   delete:
+ *       summary: Supprimer un utilisateur par id
+ *       tags: [Utilisateurs]
+ *       parameters:
+ *          - in: path
+ *            name: id
+ *            schema:
+ *              type: string
+ *            required: true
+ *            description: L'utilisateur id
+ *       responses:
+ *         200:
+ *           description: L'utilisateur a été supprimé
+ *         404:
+ *           description: L'utilisateur n'a pas été trouvé
+ */
 
 router.delete("/:id", utilisateursControleur.supprimerUnUtilisateur);
 
